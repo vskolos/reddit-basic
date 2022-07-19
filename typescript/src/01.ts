6bcadae2df8092726341240400db202261cd8162
Vsevolod Kolos <vskolos@icloud.com>
2022-07-19 20:28:57 +0300

    add ConcatFn type

--- typescript/src/01.ts
+++ typescript/src/01.ts
@@ -1,5 +1,5 @@
-function concat(a: string, b: string): string {
-  return a + b
-}
+type ConcatFn = (a: string, b: string) => string
+
+const concat: ConcatFn = (a: string, b: string): string => a + b
 
 console.log(concat('Hello ', 'World'))
