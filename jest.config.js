609127e1bcc3877b03335a5178b5a6541833fa2d
Vsevolod Kolos <vskolos@icloud.com>
2022-07-21 15:01:51 +0300

    jest config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+  },
+}
