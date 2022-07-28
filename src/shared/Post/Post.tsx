251611d6dc65d7f33d224ec93790b5051334e125
Vsevolod Kolos <vskolos@icloud.com>
2022-07-28 11:45:55 +0300

    remove unused imports

--- src/shared/Post/Post.tsx
+++ src/shared/Post/Post.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { usePostModal } from '../../hooks/usePostModal'
 import { CommentForm } from '../CommentForm'
