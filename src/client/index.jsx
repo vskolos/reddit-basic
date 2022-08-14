9d3d392d5b7669dd7764065e7252c23be96d58cf
Vsevolod Kolos <vskolos@icloud.com>
2022-08-14 11:10:45 +0300

    change variable name

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import * as ReactDom from 'react-dom'
+import * as ReactDOM from 'react-dom'
 import { App } from '../App'
 
 window.addEventListener('load', () => {
-  ReactDom.hydrate(<App />, document.getElementById('react_root'))
+  ReactDOM.hydrate(<App />, document.getElementById('react_root'))
 })
