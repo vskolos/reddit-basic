da86d8f6d159a38b21a6aa74cd3669979447ba5e
Vsevolod Kolos <vskolos@icloud.com>
2022-08-14 11:10:34 +0300

    remove jest configs

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '\\.css$': 'identity-obj-proxy',
-  },
-}
