282963a3cd521985731cce8aad0287db692e00e0
Vsevolod Kolos <vskolos@icloud.com>
2022-08-26 14:25:42 +0300

    fix env error, move auth logic to server

--- src/components/Token/Token.tsx
+++ src/components/Token/Token.tsx
@@ -1,37 +1,12 @@
-import axios from 'axios'
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate, useSearchParams } from 'react-router-dom'
-import { setCookie } from '../../utils/cookies'
-import { set as setToken } from '../../app/tokenSlice'
+import { useNavigate } from 'react-router-dom'
+import useToken from '../../hooks/useToken'
 
 export default function Token() {
-  const [searchParams, _] = useSearchParams()
-  const code = searchParams.get('code')
   const navigate = useNavigate()
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-    axios
-      .post(
-        'https://www.reddit.com/api/v1/access_token',
-        `grant_type=authorization_code&code=${code}&redirect_uri=${process.env.URI}/auth`,
-        {
-          auth: {
-            username: process.env.CLIENT_ID!,
-            password: process.env.SECRET!,
-          },
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        }
-      )
-      .then(({ data }) => {
-        setCookie('token', data.access_token)
-        dispatch(setToken(data.access_token))
-        navigate('/posts', { replace: true })
-      })
-  }, [])
+  useToken()
+  useEffect(() => navigate('/posts', { replace: true }), [])
 
   return <></>
 }
